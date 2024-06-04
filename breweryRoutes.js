6a83315a8ff2d3bf1c7e379cf0f5dedc62164876
Shajia Midhath <93853468+shajiamidhath@users.noreply.github.com>
2024-06-04 16:21:25 +0530

    Delete breweryRoutes.js

--- breweryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { searchBreweries, getBreweryDetails } = require('../controllers/breweryController');
-const router = express.Router();
-
-router.get('/search', searchBreweries);
-router.get('/:id', getBreweryDetails);
-
-module.exports = router;
