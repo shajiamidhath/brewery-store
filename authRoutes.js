5cc54089ea8d4aa0567c84b1cf1a94d5d38ef212
Shajia Midhath <93853468+shajiamidhath@users.noreply.github.com>
2024-06-04 16:22:25 +0530

    Delete authRoutes.js

--- authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const authController = require('../controllers/authController');
-const router = express.Router();
-
-router.post('/signup', signup);
-router.post('/login', login);
-
-module.exports = router;
