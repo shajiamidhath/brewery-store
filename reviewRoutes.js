851a0cb2c3bf3b00236193fe4da3bc5f28ada0b3
Shajia Midhath <93853468+shajiamidhath@users.noreply.github.com>
2024-06-04 16:23:12 +0530

    Delete reviewRoutes.js

--- reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { addReview, getReviews } = require('../controllers/reviewController');
-const auth = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.post('/', auth, addReview);
-router.get('/:breweryId', getReviews);
-
-module.exports = router;
